576744da8c61363cca3f77da9cf8e18b9cbb3135
Osiris Mariano <oslimacontato@gmail.com>
2026-01-14 06:06:00 -0300

    feat(core): implement formula-based conversion for temperatures (ref #4)

--- unidades.rb
+++ unidades.rb
@@ -1,24 +1,35 @@
 module Unidades
   DATA = {
-    COMPRIMENTO: {
-    "1" => { nome: "Quilômetro",  fator: 0.001},
-    "2" => { nome: "Metro",       fator: 1.0},
-    "3" => { nome: "Centímetros", fator: 100.0},
-    "4" => { nome: "Milímetros",  fator: 1000.0}
-  },
-
-    MASSA: {
-      "5" => {nome: "Quilograma", fator: 1.0},
-      "6" => {nome: "Grama",      fator: 1000.0},
-      "7" => {nome: "Libra",     fator: 2.20462}
+    comprimento: {
+      "1" => { nome: "Quilômetro", fator: 0.001 },
+      "2" => { nome: "Metro",      fator: 1.0 },
+      "3" => { nome: "Centímetro", fator: 100.0 },
+      "4" => { nome: "Milímetro",  fator: 1000.0 }
+    },
+    massa: {
+      "5" => { nome: "Quilograma", fator: 1.0 },
+      "6" => { nome: "Grama",      fator: 1000.0 },
+      "7" => { nome: "Libra",      fator: 2.20462 }
+    },
+    temperatura: {
+      "8" => { nome: "Celsius",    fator: 1.0 },
+      "9" => { 
+        nome: "Fahrenheit", 
+        para_base: ->(v) { (v - 32) / 1.8 },
+        da_base:   ->(v) { (v * 1.8) + 32 }
+      },
+      "10" => { 
+        nome: "Kelvin",     
+        para_base: ->(v) { v - 273.15 },
+        da_base:   ->(v) { v + 273.15 }
+      }
     }
-
   }
-  
+
   def self.listar_todas
     DATA.each do |categoria, unidades|
-      puts "\n--- #{categoria.upcase}---"
-      unidades.each { |id, info| puts "[#{id}] #{info[:nome]}"}
+      puts "\n\e[1m--- #{categoria.upcase} ---\e[0m"
+      unidades.each { |id, info| puts "[#{id}] #{info[:nome]}" }
     end
   end
 
@@ -30,10 +41,21 @@ module Unidades
   end
 
   def self.converter(valor, id_de, id_para)
-    unidade_de    = buscar_unidade(id_de)
-    unidade_para  = buscar_unidade(id_para)
+    u_de = buscar_unidade(id_de)
+    u_para = buscar_unidade(id_para)
 
-    valor_base = valor / unidade_de[:fator]
-    valor_base * unidade_para[:fator]
+    # 1. Converte a origem para a unidade BASE (Metro, Kg ou Celsius)
+    valor_base = if u_de[:para_base]
+                   u_de[:para_base].call(valor)
+                 else
+                   valor / u_de[:fator]
+                 end
+
+    # 2. Converte da unidade BASE para o destino final
+    if u_para[:da_base]
+      u_para[:da_base].call(valor_base)
+    else
+      valor_base * u_para[:fator]
+    end
   end
 end
